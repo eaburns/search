17441db560eb0dc04015c85f42a26b06c7c4afec
Ethan Burns <burns.ethan@gmail.com>
2011-09-01 09:07:33 -0400

    Specialize the openlist over the cost type.

--- search/openlist.hpp
+++ search/openlist.hpp
@@ -1,47 +1,10 @@
 #include "../structs/intpq.hpp"
 #include "../structs/binheap.hpp"
 
-template <class Ops, class Ent> class OpenList {
-public:
-	void push(Ent) = 0;
-	Ent pop(void) = 0;
-	void pre_update(Ent) = 0;
-	void post_update(Ent) = 0;
-	bool empty(void) = 0;
-};
-
-template <class Ops, class Ent> class IntOpenList : public OpenList<Ops, Ent> {
+template <class Ops, class Ent, class Cost> class OpenList {
 public:
 
-	void push(Ent e) {
-		pq.push(e);
-		Ops::setindex(e, 1);
-	}
-
-	Ent pop(void) {
-		Ent &e = pq.pop();
-		Ops::setindex(e, 0);
-		return e;
-	}
-
-	void pre_update(Ent e) {
-		pq.rm(e);
-	}
-
-	void post_update(Ent e) {
-		pq.push(e);
-	}
-
-	bool empty(void) {
-		return pq.empty();
-	}
-
-private:
-	Intpq<Ops, Ent> pq;
-};
-
-template <class Ops, class Ent> class FloatOpenList : public OpenList<Ops, Ent> {
-public:
+	enum { InitInd = -1 };
 
 	void push(Ent e) {
 		heap.push(e);
@@ -62,6 +25,47 @@ public:
 		return heap.empty();
 	}
 
+	bool mem(Ent e) {
+		return Ops::getind(e) >= 0;
+	}
+
 private:
 	Binheap<Ops, Ent> heap;
+};
+
+
+template <class Ops, class Ent> class OpenList <Ops, Ent, int> {
+public:
+
+	enum { InitInd = -1 };
+
+	void push(Ent e) {
+		pq.push(e);
+		Ops::setindex(e, 1);
+	}
+
+	Ent pop(void) {
+		Ent &e = pq.pop();
+		Ops::setindex(e, -1);
+		return e;
+	}
+
+	void pre_update(Ent e) {
+		pq.rm(e);
+	}
+
+	void post_update(Ent e) {
+		pq.push(e);
+	}
+
+	bool empty(void) {
+		return pq.empty();
+	}
+
+	bool mem(Ent e) {
+		return Ops::getind(e) >= 0;
+	}
+
+private:
+	Intpq<Ops, Ent> pq;
 };
