1ae130a7ef8755e77b958cff1ef238de4d0b9a01
Ethan Burns <burns.ethan@gmail.com>
2011-09-30 09:36:08 -0400

    Handle Nathan's scenario files as input.

--- gridpath/main.cc
+++ gridpath/main.cc
@@ -1,37 +1,53 @@
 #include "gridpath.hpp"
 #include "../search/main.hpp"
 #include "../utils/utils.hpp"
-#include <cstdio>
+#include <iostream>
+#include <string>
+#include <cmath>
+
+const float Epsilon = 0.0001;
 
 int main(int argc, char *argv[]) {
 	float ver;
-	if (scanf("version %f", &ver) != 1)
-		fatal("Failed to read version header");
+	std::string verstr;
+
+	std::cin >> verstr >> ver;
+	if (verstr != "version")
+		fatal("Expected a version header");
 
 	if (ver != 1.0)
 		fatal("Version %g is unsupported.  Latest supported version is 1.0", ver);
 
+	unsigned int lineno = 1;
 	while (!feof(stdin) && !ferror(stdin)) {
-		char map[1024];
+		std::string map;
 		unsigned int bucket;
 		unsigned int w, h;
 		unsigned int x0, y0;
 		unsigned int x1, y1;
-		float len;
+		float opt;
 
-		if (scanf("%u", &bucket) != 1)
-			break;
-		if (scanf("%s", map) != 1)	// Dangerous1
-			break;
-		if (scanf(" %u %u %u %u %u %u %f", &w, &h, &x0, &y0,
-			&x1, &y1, &len) != 7)
-			break;
+		std::cin >> bucket >> map >> w >> h >> x0 >> y0 >> x1 >> y1 >> opt;
+		lineno++;
 
-		GridPath d(map, x0, y0, x1, y1);
+		GridPath d(map.c_str(), x0, y0, x1, y1);
 		if (d.width() != w || d.height() != h)
 			fatal("Map dimensions don't match the scenario dimensions");
 
-		search<GridPath>(d, argc, argv);
+		Result<GridPath> r = search<GridPath>(d, argc, argv);
+		dfpair(stdout, "bucket", "%u", bucket);
+		dfpair(stdout, "map width", "%u", w);
+		dfpair(stdout, "map height", "%u", h);
+		dfpair(stdout, "start x", "%u", x0);
+		dfpair(stdout, "start y", "%u", y0);
+		dfpair(stdout, "finish x", "%u", x1);
+		dfpair(stdout, "finish y", "%u", y1);
+		
+		if (fabsf(r.cost - opt) > Epsilon)
+			fatal("Scenario line %d: expected optimal cost %g, got %g\n",
+				lineno, opt, r.cost);
+
+		putc('\n', stdout);
 	}
 
 	if (ferror(stdin))
