984c089360d172877d9bfaf341ea0c92e52e7dd4
Ethan Burns <burns.ethan@gmail.com>
2011-10-26 14:33:56 -0400

    utils: add pathfor program.

---
+++ utils/pathfor.cc
@@ -0,0 +1,29 @@
+#include "utils.hpp"
+#include <cstring>
+
+void usage(void);
+
+int main(int argc, char *argv[]) {
+	if (argc < 2)
+		usage();
+
+	const char *root = argv[1];
+
+	RdbAttrs attrs;
+	for (int i = 2; i < argc; i++) {
+		char *key = argv[i];
+		char *vl = strchr(key, '=');
+		if (!vl)
+			usage();
+		vl[0] = '\0';
+		vl++;
+		attrs[key] = vl;
+	}
+
+	printf("%s\n", rdbpathfor(root, attrs).c_str());
+	return 0;
+}
+
+void usage(void) {
+	fatal("Usage: pathfor <root> <key>=<value>*\n");
+}
