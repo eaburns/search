c6176a2065fa5075f5f6918d5e9a51f869c912e7
Ethan Burns <burns.ethan@gmail.com>
2011-10-31 15:55:44 -0400

    search: change the OpenList for unit-cost problems to a 2d array. 	This will allow for tie-breaking on g.

--- search/openlist.hpp
+++ search/openlist.hpp
@@ -50,36 +50,108 @@ private:
 };
 
 template<class Node> struct OpenEntry {
-	IntpqEntry<Node> node;
+	unsigned int i;
 };
 
 template <class Ops, class Node> class OpenList <Ops, Node, char> {
 public:
-	static const char *kind(void) { return "bucketed"; }
+	OpenList(void) : fill(0), min(0) { }
 
-	void push(Node *n) { pq.push(n, Ops::prio(n)); }
+	static const char *kind(void) { return "2d bucketed"; }
 
-	Node *pop(void) { return pq.pop(); }
+	void push(Node *n) {
+		unsigned long p0 = Ops::prio(n);
+ 
+		if (qs.size() <= p0)
+			qs.resize(p0+1);
 
-	void pre_update(Node*n) { pq.rm(n, Ops::prio(n)); }
+		if (p0 < min)
+			min = p0;
+
+		qs[p0].push(n, n->g);
+		fill++;
+	}
+
+	Node *pop(void) {
+		for ( ; min < qs.size() && qs[min].empty() ; min++)
+			;
+		fill--;
+		return qs[min].pop();		
+	}
+
+	void pre_update(Node*n) {
+		assert ((unsigned long) Ops::prio(n) < qs.size());
+		qs[Ops::prio(n)].rm(n, n->g);
+		fill--;
+	}
 
 	void post_update(Node *n) { push(n); }
 
-	bool empty(void) { return pq.empty(); }
+	bool empty(void) { return fill == 0; }
 
-	bool mem(Node *n) { return pq.mem(n); }
+	bool mem(Node *n) { return Ops::openentry(n).i >= 0; }
 
-	void clear(void) { pq.clear(); }
-
-	typedef IntpqEntry<Node> OpenEntry;
+	void clear(void) {
+		qs.clear();
+		min = 0;
+	}
 
 private:
-	struct Intpqops {
-		static IntpqEntry<Node> &entry(Node *n) {
-			return Ops::openentry(n).node;
+
+	struct Maxq {
+		Maxq(void) : fill(0), max(0) { }
+
+		void push(Node *n, unsigned long p) {
+			if (bkts.size() <= p)
+				bkts.resize(p+1);
+
+			if (p > max)
+				max = p;
+
+			Ops::openentry(n).i = bkts[p].size();
+			bkts[p].push_back(n);
+			fill++;
 		}
+
+		Node *pop(void) {
+			for ( ; max >= 0 && bkts[max].empty(); max--) {
+				if (max == 0)
+					break;
+			}
+			Node *n = bkts[max].back();
+			bkts[max].pop_back();
+			Ops::openentry(n).i = -1;
+			fill--;
+			return n;
+		}
+
+		void rm(Node *n, unsigned long p) {
+			assert (p < bkts.size());
+			std::vector<Node*> &bkt = bkts[p];
+
+			unsigned int i = Ops::openentry(n).i;
+			assert (i < bkt.size());
+
+			if (bkt.size() > 1) {
+				bkt[i] = bkt[bkt.size() - 1];
+				Ops::openentry(bkt[i]).i = i;
+			}
+
+			bkt.pop_back();
+			Ops::openentry(n).i = -1;
+			fill--;
+		}
+
+		bool empty(void) { return fill == 0; }
+
+		unsigned long fill;
+		unsigned int max;
+		std::vector< std::vector<Node*> > bkts;
 	};
-	Intpq<Intpqops, Node> pq;
+
+	unsigned long fill;
+	unsigned int min;
+	std::vector<Maxq> qs;
 };
 
 #endif	// _OPENLIST_HPP_
