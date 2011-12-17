6723cdc34c2e094a75b38f88bc4552aa6fbd4f2f
Ethan Burns <burns.ethan@gmail.com>
2011-12-17 17:19:19 -0500

    gridnav: small formatting changes to decrease code density.

--- gridnav/gridmap.hpp
+++ gridnav/gridmap.hpp
@@ -15,6 +15,12 @@ public:
 
 	~GridMap(void);
 
+	unsigned int width(void) const { return w; }
+
+	unsigned int height(void) const { return h; }
+
+	unsigned int size(void) const { return sz; }
+
 	unsigned int x(unsigned int loc) const { return loc % w; }
 
 	unsigned int y(unsigned int loc) const { return loc / w; }
@@ -22,12 +28,6 @@ public:
 	bool passable(unsigned int loc) const { return flags[loc] & Passable; }
 
 	unsigned int loc(unsigned int x, unsigned int y) const { return y * w + x; }
-
-	unsigned int width(void) const { return w; }
-
-	unsigned int height(void) const { return h; }
-
-	unsigned int size(void) const { return sz; }
 
 	unsigned int up(unsigned int l) const { return l - w; }
 
@@ -65,23 +65,29 @@ private:
 		unsigned int n = 0;
 
 		bool leftok = x(l) > 0 && terrainok(l, left(l));
-		if (leftok) ops[n++] = left(l);
+		if (leftok)
+			ops[n++] = left(l);
 
 		bool rightok = x(l) < w - 1 && terrainok(l, right(l));
-		if (rightok) ops[n++] = right(l);
+		if (rightok)
+			ops[n++] = right(l);
 
 		bool upok = y(l) > 0 && terrainok(l, up(l));
 		if (upok) {
 			ops[n++] = up(l);
-			if (leftok && terrainok(l, up(left(l)))) ops[n++] = up(left(l));
-			if (rightok && terrainok(l, up(right(l)))) ops[n++] = up(right(l));
+			if (leftok && terrainok(l, up(left(l))))
+				ops[n++] = up(left(l));
+			if (rightok && terrainok(l, up(right(l))))
+				ops[n++] = up(right(l));
 		}
 
 		bool downok = y(l) < h - 1 && terrainok(l, down(l));
 		if (downok) {
 			ops[n++] = down(l);
-			if (leftok && terrainok(l, down(left(l)))) ops[n++] = down(left(l));
-			if (rightok && terrainok(l, down(right(l)))) ops[n++] = down(right(l));
+			if (leftok && terrainok(l, down(left(l))))
+				ops[n++] = down(left(l));
+			if (rightok && terrainok(l, down(right(l))))
+				ops[n++] = down(right(l));
 		}
 
 		return n;
@@ -90,19 +96,27 @@ private:
 	unsigned int eightwayops(unsigned int l, int ops[]) {
 		unsigned int n = 0;
 
-		if (x(l) > 0 && terrainok(l, left(l))) ops[n++] = left(l);
-		if (x(l) < w - 1 && terrainok(l, right(l))) ops[n++] = right(l);
+		if (x(l) > 0 && terrainok(l, left(l)))
+			ops[n++] = left(l);
+		if (x(l) < w - 1 && terrainok(l, right(l)))
+			ops[n++] = right(l);
 
 		if (y(l) > 0) {
-			if (terrainok(l, up(l))) ops[n++] = up(l);
-			if (x(l) > 0 && terrainok(l, up(left(l)))) ops[n++] = up(left(l));
-			if (x(l) < w - 1 && terrainok(l, up(right(l)))) ops[n++] = up(right(l));
+			if (terrainok(l, up(l)))
+				ops[n++] = up(l);
+			if (x(l) > 0 && terrainok(l, up(left(l))))
+				ops[n++] = up(left(l));
+			if (x(l) < w - 1 && terrainok(l, up(right(l))))
+				ops[n++] = up(right(l));
 		}
 
 		if (y(l) < h - 1) {
-			if (terrainok(l, down(l))) ops[n++] = down(l);
-			if (x(l) > 0 && terrainok(l, down(left(l)))) ops[n++] = down(left(l));
-			if (x(l) < w - 1 && terrainok(l, down(right(l)))) ops[n++] = down(right(l));
+			if (terrainok(l, down(l)))
+				ops[n++] = down(l);
+			if (x(l) > 0 && terrainok(l, down(left(l))))
+				ops[n++] = down(left(l));
+			if (x(l) < w - 1 && terrainok(l, down(right(l))))
+				ops[n++] = down(right(l));
 		}
 
 		return n;
@@ -110,10 +124,14 @@ private:
 
 	unsigned int fourwayops(unsigned int l, int ops[]) {
 		unsigned int n = 0;
-		if (x(l) > 0 && terrainok(l, left(l))) ops[n++] = left(l);
-		if (x(l) < w - 1 && terrainok(l, right(l))) ops[n++] = right(l);
-		if (y(l) > 0 && terrainok(l, up(l))) ops[n++] = up(l);
-		if (y(l) < h - 1 && terrainok(l, down(l))) ops[n++] = down(l);
+		if (x(l) > 0 && terrainok(l, left(l)))
+			ops[n++] = left(l);
+		if (x(l) < w - 1 && terrainok(l, right(l)))
+			ops[n++] = right(l);
+		if (y(l) > 0 && terrainok(l, up(l)))
+			ops[n++] = up(l);
+		if (y(l) < h - 1 && terrainok(l, down(l)))
+			ops[n++] = down(l);
 		return n;
 	}
 
