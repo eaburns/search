e962cdc08e35e56ff53352e537ae66952c435630
Ethan Burns <burns.ethan@gmail.com>
2011-12-23 12:26:26 -0500

    wastar: populate the solution operator list.

--- search/wastar.hpp
+++ search/wastar.hpp
@@ -179,6 +179,8 @@ private:
 			State buf;
 			State &state = d.unpack(buf, n->packed);
 			Search<D>::res.path.push_back(state);
+			if (n->parent)
+				Search<D>::res.ops.push_back(n->op);
 		}
 	}
 
