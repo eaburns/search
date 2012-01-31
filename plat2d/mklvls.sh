6efd13285fc7a235da6d86c076ef9e34265e0139
Ethan Burns <burns.ethan@gmail.com>
2012-01-31 09:44:20 -0500

    plat2d: don't bother tie-breaking on distance for now.

--- plat2d/mklvls.sh
+++ plat2d/mklvls.sh
@@ -2,11 +2,12 @@
 
 w=25
 h=25
+n=25
 lvlgen=/home/aifs2/eaburns/src/c/mid-game/cmd/lvlgen/lvlgen
 pathfor=/home/aifs2/eaburns/src/c/search/utils/pathfor
 root=/home/aifs2/group/data/plat2d
 
-for i in `seq 0 9`; do
+for i in `seq $n`; do
 	path=$($pathfor $root width=$w height=$w num=$i)
 	echo "$lvlgen $w $h 1 -w > $path"
 	$lvlgen $w $h 1 -w > $path
