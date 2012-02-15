67cae800dc16b824d5d660c9b22c51d7d1ef6d2f
Ethan Burns <burns.ethan@gmail.com>
2012-02-15 14:44:39 -0500

    gridnav: 2poly enforces an outter bounding box.

--- gridnav/2poly.cc
+++ gridnav/2poly.cc
@@ -4,15 +4,19 @@
 
 int main(int argc, char *argv[]) {
 	GridMap map(stdin);
-	bool *blkd = new bool[map.size()];
-	for (unsigned int i = 0; i < map.size(); i++)
-		blkd[i] = false;
+	unsigned int sz = (map.width() + 2) * (map.height() + 2);
+	unsigned int stride = map.height() + 2;
+	bool *blkd = new bool[sz];
+	for (unsigned int i = 0; i < sz; i++)
+		blkd[i] = true;
 	for (unsigned int i = 0; i < map.width(); i++) {
-	for (unsigned int j = 0; j < map.height(); j++)
-		blkd[i * map.height() + j] = !map.passable(map.loc(i, j));
+		unsigned int base = (i + 1) * stride;
+
+		for (unsigned int j = 0; j < map.height(); j++)
+			blkd[base + (j + 1)] = !map.passable(map.loc(i, j));
 	}
 
-	PolyMap polys(blkd, map.width(), map.height());
+	PolyMap polys(blkd, map.width() + 2, map.height() + 2);
 	polys.output(stdout);
 
 	return 0;
