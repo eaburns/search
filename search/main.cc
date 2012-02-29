bb1a2995801ae06fdbf7239fa19786f36bef8e45
Ethan Burns <burns.ethan@gmail.com>
2012-02-29 10:06:20 -0500

    search: oops, forgot to add main.cc.

---
+++ search/main.cc
@@ -0,0 +1,9 @@
+#include "main.hpp"
+
+bool headerfooter(int argc, const char *argv[]) {
+	for (int i = 0; i < argc; i++) {
+		if (strcmp(argv[i], "-ocamlrun") == 0)
+			return false;
+	}
+	return true;
+}
