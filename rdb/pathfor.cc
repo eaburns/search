3e015915aaf23cd3860c8a545cb085e82ccdfb64
Ethan Burns <burns.ethan@gmail.com>
2012-02-29 16:25:29 -0500

    rdb: better handling of keys with spaces in pathfor.

--- rdb/pathfor.cc
+++ rdb/pathfor.cc
@@ -5,6 +5,7 @@
 void fatal(const char*, ...);
 
 void usage(void);
+int nextpair(int, char *[], std::string&, std::string&);
 
 int main(int argc, char *argv[]) {
 	if (argc < 2)
@@ -13,14 +14,14 @@ int main(int argc, char *argv[]) {
 	const char *root = argv[1];
 
 	RdbAttrs attrs;
-	for (int i = 2; i < argc; i++) {
-		char *key = argv[i];
-		char *vl = strchr(key, '=');
-		if (!vl)
-			usage();
-		vl[0] = '\0';
-		vl++;
-		attrs.push_back(key, vl);
+	argc -= 2;
+	argv+= 2;
+	while (argc > 0) {
+		std::string key, value;
+		int shift = nextpair(argc, argv, key, value);
+		argc -= shift;
+		argv += shift;
+		attrs.push_back(key.c_str(), value.c_str());
 	}
 
 	printf("%s\n", rdbpathfor(root, attrs).c_str());
@@ -29,4 +30,24 @@ int main(int argc, char *argv[]) {
 
 void usage(void) {
 	fatal("Usage: pathfor <root> <key>=<value>*\n");
+}
+
+int nextpair(int argc, char *argv[], std::string &key, std::string &value) {
+	key.clear();
+	value.clear();
+
+	for (int i = 0; i < argc; i++) {
+		char *vl =strchr(argv[i], '=');
+		if (!vl) {
+			key += argv[i];
+			key.push_back(' ');
+			continue;
+		}
+		vl[0] = '\0';
+		key += argv[i];
+		value = vl+1;
+		return i+1;
+	}
+	usage();
+	return -1;	// unreachable
 }
