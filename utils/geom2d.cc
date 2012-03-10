29dafafcc942162ee41370343fac491a9526e2c7
Ethan Burns <burns.ethan@gmail.com>
2012-03-09 19:33:47 -0500

    geom: use Epsilon instead of the more verbose std version.

--- utils/geom2d.cc
+++ utils/geom2d.cc
@@ -198,7 +198,7 @@ namespace Geom2d {
 	
 	void Poly::removecolinear(void) {
 		for (unsigned int i = 0; i < verts.size(); ) {
-			if (fabs(interangle(i) - M_PI) < std::numeric_limits<double>::epsilon())
+			if (fabs(interangle(i) - M_PI) < Epsilon)
 				verts.erase(verts.begin() + i);
 			else
 				i++;
