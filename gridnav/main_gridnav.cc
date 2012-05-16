d463f3461c03cb2ce72a0f63a01b416755185cf2
Ethan Burns <burns.ethan@gmail.com>
2012-05-16 11:46:48 -0400

    gridnav: fix a bug with octile distance instances.

--- gridnav/main_gridnav.cc
+++ gridnav/main_gridnav.cc
@@ -11,6 +11,11 @@ int main(int argc, const char *argv[]) {
 	if (fscanf(stdin, " %u %u %u %u", &x0, &y0, &xg, &yg) != 4)
 		fatal("Failed to read start and end locations");
 
+	dfpair(stdout, "start x", "%u", x0);
+	dfpair(stdout, "start y", "%u", y0);
+	dfpair(stdout, "goal x", "%u", xg);
+	dfpair(stdout, "goal y", "%u", yg);
+
 	GridNav d(&map, x0, y0, xg, yg);
 	search<GridNav>(d, argc, argv);
 	dffooter(stdout);
