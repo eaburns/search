e9106d7fc79ceb4c069cea2ffa0ef226b0339195
Ethan Burns <burns.ethan@gmail.com>
2012-08-29 10:42:17 -0400

    plat2d: exit with failure if lvlgen fails.

--- plat2d/mklvls.sh
+++ plat2d/mklvls.sh
@@ -10,6 +10,6 @@ root=/home/aifs2/group/data/plat2d
 for i in `seq $n`; do
 	path=$($pathfor $root width=$w height=$w num=$i)
 	echo "$lvlgen $w $h 1 -w -r > $path"
-	$lvlgen $w $h 1 -w -r > $path
+	$lvlgen $w $h 1 -w -r > $path || exit 1
 	sleep 1
 done
