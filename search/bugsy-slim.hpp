0c983020976ceb059198e279d95a60779f6143cf
Ethan Burns <burns.ethan@gmail.com>
2013-03-09 09:32:19 -0500

    bugsy-slim: remove the old paper citation.
    This is not the same bugsy as described in the IJCAI paper. The main
    idea is the same, but the execution is better.

--- search/bugsy-slim.hpp
+++ search/bugsy-slim.hpp
@@ -1,7 +1,4 @@
-// Best-first Utility-Guided Search Yes! From:
-// "Best-first Utility-Guided Search," Wheeler Ruml and Minh B. Do,
-// Proceedings of the Twentieth International Joint Conference on
-// Artificial Intelligence (IJCAI-07), 2007
+// Best-first Utility-Guided Search Yes!
 
 #include "../search/search.hpp"
 #include "../structs/binheap.hpp"
